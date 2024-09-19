bfc0b3376c176b7a4bb052c054fed3ef90a0afcd
cassepipe <cassepipe@ymail.com>
2024-09-19 20:16:27 +0200

    Initial commit

---
+++ src/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '../app.css';
+</script>
+
+<!-- Top-right navigation -->
+<nav class="top-nav absolute right-0 top-0 p-4">
+	<ul class="flex space-x-4">
+		<li><a href="/cv.pdf" target="_blank" class="text-gray-800 hover:text-blue-500">CV</a></li>
+		<li><a href="/contact" class="text-gray-800 hover:text-blue-500">Contact</a></li>
+		<li><a href="/about" class="text-gray-800 hover:text-blue-500">A Propos</a></li>
+	</ul>
+</nav>
+
+<div class="flex">
+	<!-- Sidebar navigation -->
+	<nav class="h-screen w-64 bg-gray-800 p-4 text-white">
+		<ul class="space-y-4">
+			<li><a href="#" class="hover:text-gray-300">Éditions</a></li>
+			<li><a href="#" class="hover:text-gray-300">Installation</a></li>
+			<li><a href="#" class="hover:text-gray-300">Projets collectifs</a></li>
+			<li><a href="#" class="hover:text-gray-300">Performances</a></li>
+		</ul>
+	</nav>
+
+	<!-- Main content slot -->
+	<slot></slot>
+</div>
---
+++ src/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<div class="mt-8 p-4">
+	<!-- Full-width image (minus the sidebar) -->
+	<img
+		src="https://www.francetvinfo.fr/pictures/JXy7WOwQcz9o180UOZk3IQBdjGg/0x61:962x601/2656x1494/filters:format(avif):quality(50)/2015/03/25/ili_pika_1.jpg"
+		alt="Ili Pika"
+		class="h-auto w-full object-cover"
+	/>
+
+	<!-- Grid of images (4 columns) -->
+	<div class="mt-4 grid grid-cols-4 gap-4">
+		<img
+			src="https://www.francetvinfo.fr/pictures/JXy7WOwQcz9o180UOZk3IQBdjGg/0x61:962x601/2656x1494/filters:format(avif):quality(50)/2015/03/25/ili_pika_1.jpg"
+			alt="Image 1"
+			class="h-auto w-full object-cover"
+		/>
+		<img
+			src="https://www.francetvinfo.fr/pictures/JXy7WOwQcz9o180UOZk3IQBdjGg/0x61:962x601/2656x1494/filters:format(avif):quality(50)/2015/03/25/ili_pika_1.jpg"
+			alt="Image 2"
+			class="h-auto w-full object-cover"
+		/>
+		<img
+			src="https://www.francetvinfo.fr/pictures/JXy7WOwQcz9o180UOZk3IQBdjGg/0x61:962x601/2656x1494/filters:format(avif):quality(50)/2015/03/25/ili_pika_1.jpg"
+			alt="Image 3"
+			class="h-auto w-full object-cover"
+		/>
+		<img
+			src="https://www.francetvinfo.fr/pictures/JXy7WOwQcz9o180UOZk3IQBdjGg/0x61:962x601/2656x1494/filters:format(avif):quality(50)/2015/03/25/ili_pika_1.jpg"
+			alt="Image 4"
+			class="h-auto w-full object-cover"
+		/>
+		<!-- Add more images if necessary -->
+	</div>
+</div>
